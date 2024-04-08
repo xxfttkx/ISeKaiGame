4f95d8a081f64829177da2581aba643b841d51d7
lauirf <592184299@qq.com>
2024-04-08 18:20:36 +0800

    change steam api

---
+++ Assets/Scripts/Base/Steam/SteamScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using Steamworks;
+public class SteamScript : MonoBehaviour
+{
+	void Start()
+	{
+		if (SteamManager.Initialized)
+		{
+			string name = SteamFriends.GetPersonaName();
+			Debug.Log(name);
+		}
+	}
+}
