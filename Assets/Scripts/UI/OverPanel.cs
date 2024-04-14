bc677bb3d3bd9c0380fc882854c1a62b29d264c5
lauirf <592184299@qq.com>
2024-04-14 22:00:22 +0800

    明天看看各个canvas把，，

---
+++ Assets/Scripts/UI/OverPanel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverPanel : MonoBehaviour
+{
+    List<int> tipsTextIndexes = new List<int>() { };
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
